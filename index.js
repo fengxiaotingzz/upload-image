8d2c3bef9a2086fc28419e28affc621f51204c1a
xiaoting feng <fengxiaotingzz@163.com>
2021-10-28 17:32:17 +0800

    上传组件

---
+++ index.js
@@ -0,0 +1,17 @@
+const installObj = {
+  react: require("./react"),
+  babelPluginSyntaxDynamicImport: require("./@babel/plugin-syntax-dynamic-import"),
+  babelCore: require("./@babel/core"),
+};
+
+const installKeys = Object.keys(installObj);
+
+const hasInstall = installKeys.every((key) => installObj[key]);
+
+if (!hasInstall) {
+  throw new Error(
+    "本插件依赖于:react、@babel/core、@babel/plugin-syntax-dynamic-import，请安装之后再使用"
+  );
+} else {
+  module.exports = require("./upload/index.js");
+}
